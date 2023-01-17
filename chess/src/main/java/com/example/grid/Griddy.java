1aba7a0016136da8252027a3508cb08404290e69
SebastianJM <buy16@uib.no>
2023-01-17 15:29:03 +0100

    added model,view,control and grid class

---
+++ chess/src/main/java/com/example/grid/Griddy.java
@@ -0,0 +1,15 @@
+package com.example.grid;
+//package pakke/navn/Class.java
+//npm install
+public class Griddy<T> {
+    private int row;
+    private int col;
+    private T tileType;
+    Griddy (int row, int col){
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                
+            }
+        }
+    }
+}
